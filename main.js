dec893cc27c63262db03f0850ab4897972e0d966
WojciechGiermanek3D <93586572+WojciechGiermanek3D@users.noreply.github.com>
2022-02-01 13:28:45 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,77 +1,77 @@
 const methods = [
   {
     name: "length",
-    function: item => {
+    func: item => {
       return item.length;
     },
     opis: "Wyswietla długość podanego stringa"
   },
   {
     name: "charAt",
-    function: (str, index) => {
-      return str.charAt(index+1);
+    func: (str, index) => {
+      return str.charAt(parseInt(index)-1);
     },
     opis: "wyswietla znak na podanej pozycji, Należy pamiętać pozycja jest liczona od zera a nie od jeden"
   },
   {
     name: "charCodeAt",
-    function: (str, index) => {
-      return str.charCodeAt();
+    func: (str, index) => {
+      return str.charCodeAt(parseInt(index)-1);
     },
     opis: "wyswietla kod ascii na podanej pozycji, Należy pamiętać pozycja jest liczona od zera a nie od jeden"
   },
   {
     name: "toUpperCase",
-    function: str => {
+    func: str => {
       return str.toUpperCase();
     },
     opis: "Konwertuje tekst do dużych znaków"
   },
   {
     name: "toLowerCase",
-    function: str => {
+    func: str => {
       return str.toLowerCase();
     },
     opis: "Konwertuje tekst do małych znaków"
   },
   {
     name: "indexOf",
-    function: (str, item) => {
+    func: (str, item) => {
       return str.indexOf(item);
     },
     opis: "wyswietla pozycję podanego znaku"
   },
   {
     name: "lastIndexOf",
-    function: (str, item) => {
+    func: (str, item) => {
       str.lastIndexOf(item);
     },
     opis: "wyswietla pozycję podanego znaku"
   },
   {
     name: "substr",
-    function: (str, position, length) => {
+    func: (str, position, length) => {
       return str.substr(position, length);
     },
     opis: "wyswietla wycinek tekstu od pozycji startowej (parametr 1), przez podaną ilośc znaków (parametr 2)"
   },
   {
     name: "substring",
-    function: (str, position, length) => {
+    func: (str, position, length) => {
       return str.substring(position, length);
     },
     opis: "wyswietla wycinek tekstu od pozycji startowej (parametr 1), przez podaną ilośc znaków (parametr 2)"
   },
   {
     name: "slice",
-    function: (str, position, length) => {
+    func: (str, position, length) => {
       return str.slice(position, length);
     },
     opis: "wyswietla wycinek tekstu od pozycji startowej (parametr 1), przez podaną ilośc znaków (parametr 2)"
   },
   {
     name: "replace",
-    function: (str, sea, rep) => {
+    func: (str, sea, rep) => {
       str.replace(sea, rep);
       return str;
     },
@@ -85,21 +85,23 @@ const strParam2 = document.querySelector(".param2");
 const OutputElement = document.querySelector(".output")
 const methDescP = document.querySelector(".methDescP")
 methods.forEach(method => {
+  console.log(method.func.length)
   const button = document.createElement("button");
   button.type = "button";
   button.classList.add("methButton","btn-secondary","border-0","m-1");
   button.innerText = method.name;
   button.addEventListener("click", () => {
+    console.log(method.func.length)
     methDescP.innerText=method.opis;
     result='no input'
-    if(method.function.length==1)
-      result=method.function(strInput.value);
-    else if(method.function.length==2)
-      result=method.function(strInput.value, strParam1.value);
-    else if(method.function.length==3)
-      result=method.function(strInput.value, strParam1.value, strParam2.value)
+    if(method.func.length==1)
+      result=method.func(strInput.value);
+    else if(method.func.length==2)
+      result=method.func(strInput.value, strParam1.value);
+    else if(method.func.length==3)
+      result=method.func(strInput.value, strParam1.value, strParam2.value)
+    console.log(result)
     OutputElement.value=result;
   });
   buttonList.appendChild(button);
 });
-
